913974b48376c6223469c77dd8ee721a79bd7876
Haji O. Haji <hajiomary188@gmail.com>
2026-06-07 12:01:44 +0300

    implements fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,21 +8,13 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
     return (
         <SafeAreaView className="flex-1 items-center justify-center bg-background">
-            <Text className="text-xl font-bold text-success">
+            <Text className="text-xl font-sans-extrabold text-primary">
                 Welcome to Nativewind!
             </Text>
 
-            <Link href='/onboarding' className="mt-4 bg-black rounded-lg px-4 py-2 text-white">Go to onboarding</Link>
-            <Link href='/(auth)/sign-in' className="mt-4 bg-black rounded-lg px-4 py-2 text-white">Go to Sign In</Link>
-            <Link href='/(auth)/sign-up' className="mt-4 bg-black rounded-lg px-4 py-2 text-white">Go to Sign up</Link>
-
-            <Link href='/subscriptions/openai-api'>OpenAI API Subscription</Link>
-            <Link href={
-                {
-                    pathname: '/subscriptions/[id]',
-                    params: { id: 'Vercel' }
-                }
-            }>Vercel Subscription</Link>
+            <Link href='/onboarding' className="mt-4 font-sans-bold bg-black rounded-lg px-4 py-2 text-white">Go to onboarding</Link>
+            <Link href='/(auth)/sign-in' className="mt-4 font-sans-bold bg-black rounded-lg px-4 py-2 text-white">Go to Sign In</Link>
+            <Link href='/(auth)/sign-up' className="mt-4 font-sans-bold bg-black rounded-lg px-4 py-2 text-white">Go to Sign up</Link>
         </SafeAreaView>
     );
 }
